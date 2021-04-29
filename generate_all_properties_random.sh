93a085b5046ddf646d1fdafaa6517c16a9634e6b
pat676 <pat676@hotmail.com>
2021-04-29 20:26:50 +0100

    Initial commit

---
+++ generate_all_properties_random.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -r ./mnist-fc/vnnlib_properties/*
+rm -r ./cifar-conv_2_255/vnnlib_properties/*
+
+cd mnist-fc
+python generate_properties.py --num_images 25 --random True --epsilons '0.03 0.05'
+cd ..
+
+cd cifar-conv_2_255
+python generate_properties.py --num_images 25 --random True --epsilons '2/255'
+cd ..
+
+cd cifar-conv_8_255
+python generate_properties.py --num_images 25 --random True --epsilons '8/255'
+cd ..
