9c581e3b4c5b811fd9c56660da4cb64b01ee42d6
pat676 <pat676@hotmail.com>
2021-04-30 14:59:04 +0100

    Added Sigmoid/Tanh

--- generate_all_properties.sh
+++ generate_all_properties.sh
@@ -3,6 +3,7 @@
 rm -r ./mnist-fc/vnnlib_properties/*
 rm -r ./cifar-conv_2_255/vnnlib_properties/*
 rm -r ./cifar-conv_8_255/vnnlib_properties/*
+rm -r ./mnist-fc-sigmoid_tanh/vnnlib_properties/*
 
 cd mnist-fc
 python generate_properties.py --num_images 25 --epsilons '0.03 0.05'
@@ -14,4 +15,8 @@ cd ..
 
 cd cifar-conv_8_255
 python generate_properties.py --num_images 100 --epsilons '8/255'
+cd ..
+
+cd mnist-fc-sigmoid_tanh
+python generate_properties.py --num_images 25 --epsilons '0.025 0.035'
 cd ..
